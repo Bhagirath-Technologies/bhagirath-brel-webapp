a8bdb557a714a1390716153b89d187ca86b737ab
anash1998 <anash.siddiqui@bhagirathtechnologies.com>
2024-03-20 16:18:10 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,33 @@
-
 let calcScrollValue = () => {
   let ScrollProgress = document.getElementById("progress");
-  let ProgressValue = document.getElementById("progress-value")
+  let ProgressValue = document.getElementById("progress-value");
   let pos = document.documentElement.scrollTop;
-let CalcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-let ScrollValue = Math.round(pos*100 / CalcHeight)
-if(pos>100){
- ScrollProgress.style.display = "grid"
-}
-else{
-    ScrollProgress.style.display = "none"
-}
-ScrollProgress.addEventListener("click",()=>{
-    document.documentElement.scrollTop = 0
-})
-}
+  let CalcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let ScrollValue = Math.round((pos * 100) / CalcHeight);
+  if (pos > 100) {
+    ScrollProgress.style.display = "grid";
+  } else {
+    ScrollProgress.style.display = "none";
+  }
+  ScrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
 window.onscroll = calcScrollValue;
-window.onload = calcScrollValue
+window.onload = calcScrollValue;
 
-const myCarousel = document.getElementById('myCarousel')
-  myCarousel.addEventListener('slid.bs.carousel', function () {
-    const activeItem = this.querySelector(".active");
-    document.querySelector(".testi-img img").src = activeItem.getAttribute("data-img");
-    document.querySelector(".testi-img .circle").style.backgroundColor = activeItem.getAttribute("data-color");
-  })
+const myCarousel = document.getElementById("myCarousel");
+myCarousel.addEventListener("slid.bs.carousel", function () {
+  const activeItem = this.querySelector(".active");
+  document.querySelector(".testi-img img").src =
+    activeItem.getAttribute("data-img");
+  document.querySelector(".testi-img .circle").style.backgroundColor =
+    activeItem.getAttribute("data-color");
+});
+
+
+
 
 
